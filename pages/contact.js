c8a4d3cb989e7ba11f4bd0f17a7838d42748919b
Bereket Mulat Mengiste <bereketmulat59@gmail.com>
2025-11-27 11:32:05 -0500

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -12,7 +12,7 @@ export default function Contact(){
         <div className="ContactInfo">
             <p>Contact Information</p>
             <a href= "https://www.linkedin.com/in/bereket-mulat-mengiste-b10156272/">LinkedIn</a>
-            <p>Email: bereketmulat89@gmail.com</p>
+            <p>Email: bereketmulatmengiste@gmail.com </p>
             <p>Phone Number: 226-501-4337</p>
             <Link href="/">Back</Link>
            
